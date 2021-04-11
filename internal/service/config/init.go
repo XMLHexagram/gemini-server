2980b40f7bfc837120c05e7b418ec0b429171f87
hexagram <xml.hexagram@gmail.com>
2021-04-11 15:22:35 +0800

    add: generate config.toml.d

--- internal/service/config/init.go
+++ internal/service/config/init.go
@@ -1,11 +1,17 @@
 package config
 
 import (
+	_ "embed"
+	"fmt"
 	"github.com/spf13/viper"
+	"os"
 	"path"
 )
 
 var configService *service
+
+//go:embed config.toml.d
+var DConfig string
 
 type service struct {
 	Config
@@ -13,10 +19,26 @@ type service struct {
 
 func Init() {
 	s := new(service)
-	viper.AddConfigPath(path.Join("configs"))
+	viper.AddConfigPath(path.Join("."))
 	viper.SetConfigName("config")
 	viper.SetConfigType("toml")
 	err := viper.ReadInConfig()
+	fmt.Println(err)
+	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		f, err := os.Create("config.toml.d")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		_, err = f.Write([]byte(DConfig))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("create config.toml.d, please rename to config.toml after configure")
+		os.Exit(1)
+
+	}
 	if err != nil {
 		panic(err)
 	}
